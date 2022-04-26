d3bdc57e2d1ebffca07e87da64d4998d7fd90990
4o4E <869951226@qq.com>
2022-04-26 22:24:35 +0800

    更新

--- src/main/kotlin/config/Config.kt
+++ src/main/kotlin/config/Config.kt
@@ -4,15 +4,21 @@ import org.bukkit.configuration.file.YamlConfiguration
 
 object Config : AbstractConfig("config.yml") {
     var debug = false
-    var duration = -1L
-    var keep = -1
-    var check = -1L
+    var update = true
+    var prefix = "&7[&aEBackupInv&7]"
+    var duration = 216000L
+    var keep = 72
+    var check = 3L
     var onDeath = false
+    var cooldown = 600L
     override fun YamlConfiguration.onLoad() {
         debug = getBoolean("debug")
+        update = getBoolean("update", true)
+        prefix = getString("prefix") ?: "&7[&aEBackupInv&7]"
         duration = getLong("duration")
         keep = getInt("keep")
         check = getLong("check")
         onDeath = getBoolean("on_death")
+        cooldown = getLong("cooldown")
     }
 }
