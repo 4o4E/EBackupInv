f97b575ad53e2c0a1f04b0d4b5870c4a0ddae1ac
4o4E <869951226@qq.com>
2022-05-02 17:01:13 +0800

    修复bug

--- src/main/kotlin/update/Update.kt
+++ src/main/kotlin/update/Update.kt
@@ -17,9 +17,8 @@ object Update : EListener {
     private const val mcbbs = "https://www.mcbbs.net/thread-1321213-1-1.html"
     private const val github = "https://github.com/4o4E/EBackupInv"
     private val jp = JsonParser()
-    private var latest: String? = null
-    private lateinit var instance: EBackupInv
-    private lateinit var nowVer: String
+    private var latest: Ver? = null
+    private lateinit var nowVer: Ver
 
     // 返回最新的版本
     private fun getLatest() = jp.parse(URL(url).readText())
@@ -27,31 +26,29 @@ object Update : EListener {
         .asJsonObject["tag_name"]
         .asString!!
 
-    private fun String.asVersion() = replace(".", "").toInt()
-
     fun init() {
-        instance = EBackupInv.instance
-        nowVer = instance.description.version
+        val instance = EBackupInv.instance
+        nowVer = instance.description.version.asVer()
         register()
         runTaskTimerAsync(0, 20 * 60 * 60 * 6) {
             if (!Config.update) return@runTaskTimerAsync
             runCatching {
-                val v = getLatest()
-                val now = instance.description.version
-                if (v.asVersion() > now.asVersion()) {
-                    latest = v
-                    info(
-                        """&f插件有更新哦, 当前版本: &c$nowVer&f, 最新版本: &a$latest
-                            |&f更新发布于:&b $mcbbs
-                            |&f开源于:&b $github
-                        """.trimMargin()
-                    )
-                    return@runCatching
+                getLatest().asVer().let {
+                    latest = it
+                    if (it > nowVer) {
+                        info(
+                            """&f插件有更新哦, 当前版本: &c$nowVer&f, 最新版本: &a$latest
+                                |&f更新发布于:&b $mcbbs
+                                |&f开源于:&b $github
+                            """.trimMargin()
+                        )
+                        return@runCatching
+                    }
+                    info("当前版本: &a${nowVer}已是最新版本")
                 }
             }.onFailure {
                 warn("检查版本更新时出现异常, 若需要手动更新请前往&b $mcbbs")
             }
-            info("当前版本: &a${nowVer}已是最新版本")
         }
     }
 
@@ -64,5 +61,21 @@ object Update : EListener {
                 |&f开源于:&b $github
             """.trimMargin()
         )
+    }
+
+    private fun String.asVer() =
+        split(".").run {
+            Ver(get(0).toInt(), get(1).toInt(), get(2).toInt(),)
+        }
+
+    private data class Ver(val fv: Int, val sv: Int, val tv: Int, ) {
+        override fun toString() = "$fv.$sv.$tv"
+        operator fun compareTo(v: Ver): Int {
+            val f = fv - v.fv
+            if (f != 0) return f
+            val s = sv - v.sv
+            if (s != 0) return s
+            return tv - v.tv
+        }
     }
 }
